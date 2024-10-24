70420e63deeaac60e2c627ad59b5f0bc67ac3949
alex86590212 <a.eroglu@student.rug.nl>
2024-10-24 16:54:04 +0200

    -

--- autoop/core/ml/artifact.py
+++ autoop/core/ml/artifact.py
@@ -14,6 +14,17 @@ class Artifact(BaseModel, ABC):
     def id(self) -> str:
         encoded_path = base64.b64encode(self._asset_path.encode()).decode()
         return f"{encoded_path}:{self._version}"
+    
+    @property
+    def details(self) -> dict:
+        """Returns the details of the artifact as a dictionary."""
+        return {
+            "asset_path": self._asset_path,
+            "version": self._version,
+            "name": self.name,
+            "type": self.type,
+            "has_data": self.data is not None
+        }
 
     @abstractmethod
     def read(self) -> Optional[bytes]:
