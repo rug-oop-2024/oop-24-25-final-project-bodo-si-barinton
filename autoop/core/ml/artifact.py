a0ef5c167b3a77d68a515e608c518287a9a2ce5f
alex86590212 <a.eroglu@student.rug.nl>
2024-10-24 16:41:17 +0200

    implemented methods in artifact

--- autoop/core/ml/artifact.py
+++ autoop/core/ml/artifact.py
@@ -11,7 +11,18 @@ class Artifact(BaseModel):
 
     @property
     def id(self) -> str:
-        encoded_path = base64.b64encode(self.asset_path.encode()).decode()
-        return f"{encoded_path}:{self.version}"
+        encoded_path = base64.b64encode(self._asset_path.encode()).decode()
+        return f"{encoded_path}:{self._version}"
 
+    def read(self) -> Optional[bytes]:
+        """Returns the stored data in bytes if available."""
+        if self.data is None:
+            raise ValueError("No data available in this artifact.")
+        return self.data
 
+    def save(self, data: bytes) -> None:
+        """Saves the provided data in the artifact."""
+        self.data = data
+
+    class Config:
+        underscore_attrs_are_private = True
