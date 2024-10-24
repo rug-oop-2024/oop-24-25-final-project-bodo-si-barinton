f159e79bdfb4101a369344bcb5d1a11b227bebf9
alex86590212 <a.eroglu@student.rug.nl>
2024-10-24 17:04:09 +0200

    Dataset that splits data

--- autoop/core/ml/dataset.py
+++ autoop/core/ml/dataset.py
@@ -2,6 +2,8 @@ from autoop.core.ml.artifact import Artifact
 from abc import ABC, abstractmethod
 import pandas as pd
 import io
+from sklearn.model_selection import train_test_split
+from typing import Optional, Tuple
 
 class Dataset(Artifact):
 
@@ -9,7 +11,7 @@ class Dataset(Artifact):
         super().__init__(type="dataset", *args, **kwargs)
 
     @staticmethod
-    def from_dataframe(data: pd.DataFrame, name: str, asset_path: str, version: str="1.0.0"):
+    def from_dataframe(data: pd.DataFrame, name: str, asset_path: str, version: str = "1.0.0"):
         return Dataset(
             name=name,
             asset_path=asset_path,
@@ -22,7 +24,12 @@ class Dataset(Artifact):
         csv = bytes.decode()
         return pd.read_csv(io.StringIO(csv))
     
-    def save(self, data: pd.DataFrame) -> bytes:
-        bytes = data.to_csv(index=False).encode()
-        return super().save(bytes)
-    
+    def save(self, data: pd.DataFrame) -> None:
+        self.data = data.to_csv(index=False).encode()
+
+    def split_data(self, test_size: float = 0.2, random_state: Optional[int] = None) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        """Splits the tabular data into training and testing sets.
+        """
+        df = self.read()
+        train_df, test_df = train_test_split(df, test_size=test_size, random_state=random_state)
+        return train_df, test_df
