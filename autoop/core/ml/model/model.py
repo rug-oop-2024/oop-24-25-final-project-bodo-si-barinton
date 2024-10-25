fec305b74035724a54197f698786e831cc97aab1
alex86590212 <a.eroglu@student.rug.nl>
2024-10-25 18:44:21 +0200

    Base Model implemented

--- autoop/core/ml/model/model.py
+++ autoop/core/ml/model/model.py
@@ -3,8 +3,30 @@ from abc import abstractmethod
 from autoop.core.ml.artifact import Artifact
 import numpy as np
 from copy import deepcopy
-from typing import Literal
+from typing import Literal, Optional
+from pydantic import PrivateAttr, Field
 
-class Model:
-    pass # your code (attribute and methods) here
+class Model(Artifact):
+
+    _parameters: dict = PrivateAttr(default_factory=dict)
+    model_type : Literal["classification" , "regression"]
+
+    @property
+    def parameters(self):
+        return deepcopy(self._parameters)
+    
+    @abstractmethod
+    def fit(self, observations : np.ndarray, ground_truth : np.ndarray) -> None:
+        pass
+
+    @abstractmethod
+    def predict(self, observations : np.ndarray) -> np.ndarray:
+        pass
+
+    def read(self) -> Optional[bytes]:
+        return self._data
+    
+    def save(self, data : bytes) -> None:
+        self._data = data
+
     
