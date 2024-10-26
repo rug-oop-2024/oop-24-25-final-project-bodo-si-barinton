6ef4f6c0530bc844389e37c9a6d0026f2e631d4f
alex86590212 <a.eroglu@student.rug.nl>
2024-10-26 14:05:01 +0200

    fixed init py and implemented multiple linear instead of linear regression

---
+++ autoop/core/ml/model/regression/__init__.py
@@ -0,0 +1,1 @@
+from autoop.core.ml.model.regression.linearregression import MultipleLinearRegression
--- autoop/core/ml/model/regression/linearregression.py
+++ autoop/core/ml/model/regression/linearregression.py
@@ -1,18 +1,26 @@
-from autoop.core.ml.model.model import Model, PrivateAttr
+from autoop.core.ml.model.model import Model
 import numpy as np
-from sklearn.linear_model import LinearRegression as LS
 
 
-class LinearRegression(Model):
-    _model = LS()
-    _is_trained : bool = PrivateAttr(default = False)
 
-    def fit(self, observations : np.ndarray, ground_truth : np.ndarray) -> None:
-        self._model.fit(observations, ground_truth)
-        self._parameters = self._model.get_params()
-        self._is_trained = True
+class MultipleLinearRegression(Model):
+    
+    def __init__(self, **kwargs):
+        # Initialize the base class and set the model type to "regression"
+        super().__init__(model_type="regression", **kwargs)
+        
+    def fit(self, observations: np.ndarray, ground_truth: np.ndarray) -> None:
+        X = np.hstack((np.ones((observations.shape[0], 1)), observations))
+        y = ground_truth
+
+        X_transpose = np.transpose(X)
+        self._parameters["coefficients"] = (
+            np.linalg.pinv(X_transpose @ X) @ X_transpose @ y
+        )
 
     def predict(self, observations: np.ndarray) -> np.ndarray:
-        if not self.is_trained:
-            raise ValueError("The model must be trained before making predictions.")
-        return self._model.predict(observations)
+        X = np.hstack((np.ones((observations.shape[0], 1)), observations))
+        coefficiants = self._parameters["coefficients"]
+        prediction = np.dot(X, coefficiants)
+
+        return prediction
