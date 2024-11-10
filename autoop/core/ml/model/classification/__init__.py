fcc1249fdbfe6c9a40733af0abe43e28f88cd5d1
alex86590212 <a.eroglu@student.rug.nl>
2024-11-10 20:35:05 +0100

    style

--- autoop/core/ml/model/classification/__init__.py
+++
@@ -1,5 +0,0 @@
-from autoop.core.ml.model.classification.bayes import BayesClassification
-from autoop.core.ml.model.classification.logistic_regression import (
-    LogisticClassification,
-)
-from autoop.core.ml.model.classification.svm import SVM
--- autoop/core/ml/model/regression/__init__.py
+++
@@ -1,7 +0,0 @@
-from autoop.core.ml.model.regression.decisiontreeregression import (
-    DecisionTreeRegressor
-)
-from autoop.core.ml.model.regression.lasso import LassoRegression
-from autoop.core.ml.model.regression.linearregression import (
-    MultipleLinearRegression
-)
